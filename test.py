cd1f291decb9ee3720592c43151b8af5c433131a
Volk <j_knz12@hotmail.com>
2026-03-11 12:04:56 +1000

    Started testing pydicom within test.py

---
+++ test.py
@@ -0,0 +1,52 @@
+import pydicom
+import os
+import shutil
+from pydicom.uid import generate_uid
+
+anonymizeKeyword = [
+    "PatientName",
+    "PatientID",
+    "PatientBirthDate",
+    "PatientSex",
+    "PatientAge",
+    "PatientWeight",
+    "InstitutionName",
+    "InstitutionAddress",
+    "ReferringPhysicianName",
+    "OperatorsName",
+    "StationName",
+    "DeviceSerialNumber",
+    "AccessionNumber",
+    "StudyID",
+    "StudyDate",
+    "SeriesDate",
+    "AcquisitionDate",
+    "StudyTime",
+    "SeriesTime",
+    "AcquisitionTime",
+    "StudyDescription",
+    "SeriesDescription",
+    "ProtocolName",
+    "PerformedProcedureStepID",
+    "PerformedProcedureStepDescription",
+]
+
+fileInput = r"D:\VIDEO PROJECT CUS SIZE BIG\MRI - HG\DICOM"
+
+## Get the number of .dcm files inside the DICOM folder
+files = os.listdir(fileInput)
+
+print(len(files))
+
+
+# ds = pydicom.dcmread(input)
+
+# # print(ds)
+
+# for elem in ds:
+#     if elem.keyword in anonymizeKeyword:
+#         print("BEFORE ANON:", elem.keyword, "=", elem.value)
+#         elem.value = ""
+#         print("AFTER ANON:", elem.keyword, "=", elem.value)
+# print(ds.PatientID)
+# print(ds.PatientBirthDate)
